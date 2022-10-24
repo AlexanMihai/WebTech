0d858aa9e153453ccba4cea33f8afac4070a854f
AlexanMihai <101118426+AlexanMihai@users.noreply.github.com>
2022-10-24 19:13:31 +0300

    Add files via upload

---
+++ homework-1-2020/main/app.js
@@ -0,0 +1,25 @@
+const distance = (a, b) => {   
+    if ((typeof a !== 'string') || (typeof b !== 'string') ){ 
+        throw new Error('InvalidType')
+    }  
+    else
+    {
+    if (!a.length) return a.length;
+    if (!b.length) return b.length;
+    const arr = [];
+        for (let i = 0; i <= b.length; i++) {
+        arr[i] = [i];
+        for (let j = 1; j <= a.length; j++) {
+            arr[i][j] = i === 0 ? j : Math.min(
+                                                arr[i - 1][j] + 1,
+                                                arr[i][j - 1] + 1,
+                                                arr[i - 1][j - 1] + (a[j - 1] === b[i - 1] ? 0 : 1)
+                                            );
+        }
+        }
+        return arr[b.length][a.length]; 
+    }
+}
+
+
+module.exports.distance = distance
